c63a2f9d122b32656060b7d87a7c685f2aad616a
Samolivyu <samolivyumanki@outlook.com>
2023-06-18 17:52:47 +0300

    Assignment Done!

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+console.log(cats);
+
+function reset() {
+  cats.length = 0;
+  cats.push('Milo', 'Otis', 'Garfield');
+}
+
+reset();
+cats.push("Ralph");
+console.log(cats);
+
+
+reset();
+cats.unshift ("Bob");
+console.log(cats);
+
+
+reset();
+cats.shift ();
+console.log(cats);
+
+reset();
+cats.slice ("Broom");
+console.log(cats);
+
+reset ();
+cats.pop
+console.log (cats);
+
+
+reset();
+cats.shift;
+console.log(cats);
+
