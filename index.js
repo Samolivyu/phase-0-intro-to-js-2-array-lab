eeb36b82146ff87d3b79b9611c27895ee5b12526
Samolivyu <samolivyumanki@outlook.com>
2023-06-18 17:57:47 +0300

    Assignment done+ changes.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const cats = ["Milo", "Otis", "Garfield"];
 console.log(cats);
+//init push value.
 
 function reset() {
   cats.length = 0;
@@ -9,27 +10,28 @@ function reset() {
 reset();
 cats.push("Ralph");
 console.log(cats);
-
+//destructivelyAppendCat
 
 reset();
 cats.unshift ("Bob");
 console.log(cats);
-
+//destructivelyPrependCat
 
 reset();
 cats.shift ();
 console.log(cats);
+//destructivelyRemoveLastCat
 
 reset();
 cats.slice ("Broom");
 console.log(cats);
+//destructivelyRemoveFirstCat
 
 reset ();
 cats.pop
 console.log (cats);
-
-
+//appendCat
 reset();
 cats.shift;
 console.log(cats);
-
+//prependCat
